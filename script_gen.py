234494fd561988e0c3b0d9d96baea53783bd8cdf
Rishov Chatterjee <garpchoo@gmail.com>
2019-03-22 00:50:51 -0700

    Adding edits

--- script_gen.py
+++ script_gen.py
@@ -1,4 +1,5 @@
-from os import listdir, system
+from os import listdir, system, getcwd
 from os.path import isfile, join
 nb_path = system("cd nb")
-onlyfiles = [f for f in listdir(nb_path) if isfile(join(nb_path, f))] 
+onlyfiles = [f for f in listdir(nb_path) if isfile(join(nb_path, f))]
+print(onlyfiles)
