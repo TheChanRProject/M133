9eccca751c54fcf35d366c5e06122ea744719873
Rishov Chatterjee <garpchoo@gmail.com>
2019-03-22 01:38:36 -0700

    Adding edits

--- script_gen.py
+++ script_gen.py
@@ -4,5 +4,9 @@ from pathlib import Path
 Path.cwd()
 onlyfiles = [f for f in listdir('/Volumes/DSDRIVE/DS/Python/Splash/M133/nb') if isfile(join('/Volumes/DSDRIVE/DS/Python/Splash/M133/nb', f))]
 print(onlyfiles)
-file_names = [i[:-6] for i in onlyfiles]
+file_names = [i[:-6].replace(' ', '-') for i in onlyfiles]
 print(file_names)
+py_names = [i+'.py' for i in file_names]
+print(py_names)
+for i in py_names:
+    system(f"touch {i} && mv {i} src")
