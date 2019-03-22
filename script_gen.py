c30fe8c67ee6e63aac11cd223222d6306dd5078c
Rishov Chatterjee <garpchoo@gmail.com>
2019-03-22 00:59:03 -0700

    Adding edits

--- script_gen.py
+++ script_gen.py
@@ -1,5 +1,7 @@
 from os import listdir, system, getcwd
 from os.path import isfile, join, dirname
-nb_path = 
+getcwd()
+nb_path = dirname('/Volumes/DSDRIVE/DS/Python/Splash/M133' + '/nb')
+print(nb_path)
 onlyfiles = [f for f in listdir(nb_path) if isfile(join(nb_path, f))]
 print(onlyfiles)
